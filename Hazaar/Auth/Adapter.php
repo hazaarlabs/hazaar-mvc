ca437f26821a66fbcc08fe637ee44cdee901ea5c
Jamie Carl <jamie@hazaarlabs.com>
2016-05-31 16:25:06 +1000

    Minor updates for PHP7.0 compat

--- Hazaar/Auth/Adapter.php
+++ Hazaar/Auth/Adapter.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file        Auth/Adapter.php
  *
@@ -8,135 +9,136 @@
  */
 
 /**
- * @brief       User authentication namespace
+ * @brief User authentication namespace
  */
 namespace Hazaar\Auth;
 
 /**
- * @brief       Abstract authentication adapter
+ * @brief Abstract authentication adapter
  */
 abstract class Adapter implements Adapter\_Interface, \ArrayAccess {
 
     protected $session;
 
-    private   $identity;
+    private $identity;
 
-    private   $credential;
+    private $credential;
+    
+    // Extra data fields to store from the user record
+    private $extra = array();
 
-    //Extra data fields to store from the user record
-    private       $extra                       = array();
+    public static $credential_encryption = 'none';
 
-    static public $credential_encryption       = 'none';
+    public static $credential_encryption_count = 1;
 
-    static public $credential_encryption_count = 1;
+    public static $autologin_period = 3600;
 
-    static public $autologin_period            = 3600;
-
-    static public $autologin_cookie            = 'siteAuth';
+    public static $autologin_cookie = 'siteAuth';
 
     function __construct() {
 
-        $this->session = new \Hazaar\Session(array('use_pragma' => FALSE));
-
+        $this->session = new \Hazaar\Session(array(
+            'use_pragma' => FALSE
+        ));
+    
     }
 
     public function setIdentity($identity) {
 
         $this->identity = $identity;
-
+    
     }
 
     public function setCredential($credential) {
 
         $this->credential = $credential;
-
+    
     }
 
     public function getIdentity() {
 
         return $this->identity;
-
+    
     }
 
     public function getCredential($credential = NULL) {
 
-        if(! $credential)
+        if (!$credential)
             $credential = $this->credential;
-
-        /*
+            
+            /*
          * Loop for added obfuscation
          */
         for($i = 1; $i <= Adapter::$credential_encryption_count; $i++) {
-
-            switch(Adapter::$credential_encryption) {
+            
+            switch (Adapter::$credential_encryption) {
                 case 'md5' :
                     $credential = md5($credential);
-
+                    
                     break;
                 case 'sha' :
                     $credential = sha1($credential);
-
+                    
                     break;
             }
-
         }
-
+        
         return $credential;
-
+    
     }
 
     /*
-     * Supported encryption is md5, sha and none.  The count value can be used to employ further obfuscation
-     * by running the encryption method multiple times.  Twice is usually more than enough.
+     * Supported encryption is md5, sha and none. The count value can be used to employ further obfuscation
+     * by running the encryption method multiple times. Twice is usually more than enough.
      */
     static public function setCredentialEncryption($method, $count = 1) {
 
         Adapter::$credential_encryption = $method;
-
+        
         Adapter::$credential_encryption_count = $count;
-
+    
     }
 
     public function setDataFields(array $fields) {
 
         $this->extra = $fields;
-
+    
     }
 
-    public function & get($key) {
+    public function &get($key) {
 
         return $this->session->get($key);
-
+    
     }
 
-    public function & __get($key) {
+    public function &__get($key) {
 
         return $this->get($key);
-
+    
     }
 
     public function __set($key, $value) {
 
         return $this->set($key, $value);
-
+    
     }
 
     public function __isset($key) {
 
         return $this->has($key);
-
+    
     }
 
     public function set($key, $value) {
 
         $this->session->set($key, $value);
-
+    
     }
 
     public function has($key) {
 
         return $this->session->has($key);
-
+    
     }
 
     public function authenticate($identity = NULL, $credential = NULL, $autologin = FALSE) {
@@ -144,62 +146,59 @@ abstract class Adapter implements Adapter\_Interface, \ArrayAccess {
         /*
          * Save the authentication data
          */
-        if($identity)
+        if ($identity)
             $this->setIdentity($identity);
-
-        if($credential)
+        
+        if ($credential)
             $this->setCredential($credential);
-
+        
         $auth = $this->queryAuth($this->getIdentity(), $this->extra);
-
+        
         /*
          * First, make sure the identity is correct.
          */
-        if($auth && $auth['identity'] == $this->getIdentity()) {
-
+        if ($auth && $auth['identity'] == $this->getIdentity()) {
+            
             /*
              * Check the credentials
              */
-            if($auth['credential'] == $this->getCredential()) {
-
-                if(array_key_exists('data', $auth))
+            if ($auth['credential'] == $this->getCredential()) {
+                
+                if (array_key_exists('data', $auth))
                     $this->session->setValues($auth['data']);
-
+                
                 $this->session->identity = md5($identity);
-
-                if($autologin && Adapter::$autologin_period > 0) {
-
+                
+                if ($autologin && Adapter::$autologin_period > 0) {
+                    
                     /*
                      * $credential should be encrypted, as stored in the datasource (ie: database), so we
-                     * md5 that to totally obscure it.  If it is not encrypted then it is not being
+                     * md5 that to totally obscure it. If it is not encrypted then it is not being
                      * stored encrypted and the developer should re-think their auth strategy but
                      * we offer some minor protection from that stupidity here.
                      */
                     $data = array(
                         'identity' => $identity,
-                        'hash'     => md5($auth['credential'])
+                        'hash' => md5($auth['credential'])
                     );
-
+                    
                     $cookie = $this->getAutologinCookieName();
-
+                    
                     setcookie($cookie, http_build_query($data), time() + Adapter::$autologin_period, \Hazaar\Application::path());
-
                 }
-
+                
                 return TRUE;
-
             }
-
         }
-
+        
         return FALSE;
-
+    
     }
 
     public function getUserData() {
 
         return $this->session->toArray();
-
+    
     }
 
     public function authenticated() {
@@ -207,114 +206,107 @@ abstract class Adapter implements Adapter\_Interface, \ArrayAccess {
         /*
          * Because we're nasty, we even make sure the identity is 32 characters so it's likely an MD5 hash
          */
-        if($this->session->has('identity') && strlen($this->session->identity) == 32) {
-
+        if ($this->session->has('identity') && strlen($this->session->identity) == 32) {
+            
             return TRUE;
-
-        } elseif($this->canAutoLogin()) {
-
+        } elseif ($this->canAutoLogin()) {
+            
             /*
              * If we've got a cookie set, use the identity to look up credentials
              */
             $cookie = $this->getAutologinCookieName();
-
+            
             parse_str($_COOKIE[$cookie]);
-
-            if($auth = $this->queryAuth($identity, $this->extra)) {
-
+            
+            if ($auth = $this->queryAuth($identity, $this->extra)) {
+                
                 /*
                  * Check the cookie credentials against the ones we just got from the adapter
                  */
-                if($identity == $auth['identity'] && $hash == md5($auth['credential'])) {
-
-                    if(array_key_exists('data', $auth)) {
-
+                if ($identity == $auth['identity'] && $hash == md5($auth['credential'])) {
+                    
+                    if (array_key_exists('data', $auth)) {
+                        
                         $this->session->setValues($auth['data']);
-
                     }
-
+                    
                     $this->session->identity = md5($identity);
-
+                    
                     return TRUE;
-
                 }
-
             }
-
         }
-
+        
         return FALSE;
-
+    
     }
 
     public function deauth() {
 
         $this->session->clear(TRUE);
-
+        
         $cookie = $this->getAutologinCookieName();
-
-        if(isset($_COOKIE[$cookie])) {
-
+        
+        if (isset($_COOKIE[$cookie])) {
+            
             unset($_COOKIE[$cookie]);
-
+            
             setcookie($cookie, NULL, time() - 3600, \Hazaar\Application::path());
-
         }
-
+        
         return TRUE;
-
+    
     }
 
     private function canAutoLogin() {
 
         $cookie = $this->getAutologinCookieName();
-
+        
         return Adapter::$autologin_period > 0 && isset($_COOKIE[$cookie]);
-
+    
     }
 
     private function getAutologinCookieName() {
 
         $cookie = Adapter::$autologin_cookie;
-
+        
         return $cookie;
-
+    
     }
 
     /**
      * Array Access Methods
      *
-     * These methods allows accessing user data as array attributes of the auth object.  These methods do not allow this
+     * These methods allows accessing user data as array attributes of the auth object. These methods do not allow this
      * data to be modified in any way.
      */
-
     public function offsetExists($key) {
 
         return $this->session->has($key);
-
+    
     }
 
-    public function & offsetGet($key) {
+    public function &offsetGet($key) {
 
-        if($this->session->has($key))
+        if ($this->session->has($key))
             return $this->session->get($key);
-
-        $result = NULL; //Required to return variables by reference
-
+        
+        $result = NULL; // Required to return variables by reference
+        
         return $result;
-
+    
     }
 
     public function offsetSet($key, $value) {
 
         $this->session->set($key, $value);
-
+    
     }
 
     public function offsetUnset($key) {
 
         $this->session->unset($key);
-
+    
     }
 
 }
--- Hazaar/Cache/Cache.php
+++ Hazaar/Cache/Cache.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file        Cache.php
  *
@@ -8,145 +9,149 @@
  *
  * @version     $Id: Application.php 24593 2012-08-29 20:35:02Z jamie $
  */
-
 namespace Hazaar;
 
 /**
- * @brief       The cache frontend
+ * @brief The cache frontend
  */
 class Cache implements \ArrayAccess {
 
     protected $options;
 
-    private   $use_cache = TRUE;
+    private $use_cache = TRUE;
 
-    private   $backend;
+    private $backend;
 
     /**
      * Cache object constructor
      *
-     * @param string $backend The name of the backend to use.  Currently 'apc', 'file', 'memcached', 'session' and
-     * 'sqlite' are supported.
-     *
-     * @param array $options Options to pass on to the backend.  These will also be specific to the backend
-     * you are using.
-     *
-     * @param string $namespace The namespace to use for grouping stored data.
-     *
+     * @param string $backend
+     *            The name of the backend to use. Currently 'apc', 'file', 'memcached', 'session' and
+     *            'sqlite' are supported.
+     *            
+     * @param array $options
+     *            Options to pass on to the backend. These will also be specific to the backend
+     *            you are using.
+     *            
+     * @param string $namespace
+     *            The namespace to use for grouping stored data.
+     *            
      * @throws Cache\Exception\InvalidBackend
      *
      * @throws Cache\Exception\InvalidFrontend
      */
     function __construct($backend = NULL, $options = array(), $namespace = 'default') {
 
-        if(! $backend)
+        if (!$backend)
             $backend = in_array('apc', get_loaded_extensions()) ? 'apc' : 'session';
-
-        if(is_array($backend)) {
-
+        
+        if (is_array($backend)) {
+            
             /**
              * If we get an array then we're using the chain backend.
              */
             $this->backend = new Cache\Backend\Chain($backend, $options);
-
         } else {
-
+            
             /*
              * Load the backend and with backend options and send it to the frontend
              */
             $backendClass = '\\Hazaar\\Cache\\Backend\\' . ucfirst($backend);
-
-            if(! class_exists($backendClass))
+            
+            if (!class_exists($backendClass))
                 throw new Cache\Exception\InvalidBackend($backendClass);
-
+            
             $this->backend = new $backendClass($options, $namespace);
-
         }
-
-        if(! $this->backend instanceof Cache\Backend)
+        
+        if (!$this->backend instanceof Cache\Backend)
             throw new Cache\Exception\InvalidBackend($backendClass);
-
+        
         $this->options = &$this->backend->options;
-
+        
         $this->configure(array(
-                             'lifetime'   => 3600,
-                             'use_pragma' => TRUE
-                         ));
-
+            'lifetime' => 3600,
+            'use_pragma' => TRUE
+        ));
+        
         /**
          * Cache skip
          *
          * Check for a Pragma header to see if we should skip loading from cache.
          */
-        if($this->options->use_pragma && function_exists('apache_request_headers')) {
-
+        if ($this->options->use_pragma && function_exists('apache_request_headers')) {
+            
             $headers = apache_request_headers();
-
-            if(array_key_exists('Pragma', $headers) && $headers['Pragma'] == 'no-cache')
+            
+            if (array_key_exists('Pragma', $headers) && $headers['Pragma'] == 'no-cache')
                 $this->use_cache = FALSE;
-
         }
-
+    
     }
 
     public function __destruct() {
 
-        if(method_exists($this->backend, 'close'))
+        if (method_exists($this->backend, 'close'))
             $this->backend->close();
-
+    
     }
 
     protected function configure($options) {
 
         $this->options->enhance($options);
-
+    
     }
 
     /**
      * Retrieve a value from cache storage.
      *
-     * @param mixed $key The reference key used to store the value.
-     *
-     * @param bool $default If the value doesn't exist, this default will be returned instead.
-     *
-     * @param bool $save_default If the value doesn't exist and a default is specified, save that default to cache.
-     *
+     * @param mixed $key
+     *            The reference key used to store the value.
+     *            
+     * @param bool $default
+     *            If the value doesn't exist, this default will be returned instead.
+     *            
+     * @param bool $save_default
+     *            If the value doesn't exist and a default is specified, save that default to cache.
+     *            
      * @return mixed The value that was stored in cache.
      */
-    public function & get($key, $default = FALSE, $save_default = FALSE) {
+    public function &get($key, $default = FALSE, $save_default = FALSE) {
 
-        if(! $this->use_cache)
+        if (!$this->use_cache)
             return $default;
-
+        
         $result = $this->backend->get($key);
-
-        if(! $this->backend->can('store_objects'))
+        
+        if (!$this->backend->can('store_objects'))
             $result = unserialize($result);
-
-        if($result == FALSE) {
-
-            if($save_default === TRUE)
+        
+        if ($result == FALSE) {
+            
+            if ($save_default === TRUE)
                 $this->set($key, $default);
-
+            
             return $default;
-
         }
-
+        
         return $result;
-
+    
     }
 
     /**
      * Store a value in the cache using the current cache backend.
      *
-     * @param mixed $key The reference key under which to store the value.
-     *
-     * @param mixed $value The value that should be stored.  Values can be pretty much anything including integers, booleans,
-     * strings and any object that can be serialised.
-     *
-     * @param mixed $timeout The number of seconds after which the value becomes invalid.  If not set the global
-     * 'lifetime' option is used.  Set a value of '-1' to indicate that the value should never timeout.
-     *
+     * @param mixed $key
+     *            The reference key under which to store the value.
+     *            
+     * @param mixed $value
+     *            The value that should be stored. Values can be pretty much anything including integers, booleans,
+     *            strings and any object that can be serialised.
+     *            
+     * @param mixed $timeout
+     *            The number of seconds after which the value becomes invalid. If not set the global
+     *            'lifetime' option is used. Set a value of '-1' to indicate that the value should never timeout.
+     *            
      * @return boolean Boolean indicating whether or not the operation succeeded.
      */
     public function set($key, $value, $timeout = NULL) {
@@ -154,130 +159,129 @@ class Cache implements \ArrayAccess {
         /*
          * If the backend can't store objects, serialize the value.
          */
-        if(! $this->backend->can('store_objects'))
+        if (!$this->backend->can('store_objects'))
             $value = serialize($value);
-
-        if($timeout === NULL && $this->options->has('lifetime'))
+        
+        if ($timeout === NULL && $this->options->has('lifetime'))
             $timeout = $this->options->get('lifetime');
-
+        
         return $this->backend->set($key, $value, $timeout);
-
+    
     }
 
     /**
      * Check if a stored value exists.
      *
-     * @param mixed $key The value key to check for.
-     *
+     * @param mixed $key
+     *            The value key to check for.
+     *            
      * @return boolean Returns TRUE or FALSE indicating if the value is stored.
      */
     public function has($key) {
 
         return $this->backend->has($key);
-
+    
     }
 
     /**
      * Removes a stored value
      *
-     * @param mixed $key The key of the value to remove
-     *
+     * @param mixed $key
+     *            The key of the value to remove
+     *            
      * @return boolean tRUE indicates the value existed and was removed. FALSE otherwise.
      */
     public function remove($key) {
 
         return $this->backend->remove($key);
-
+    
     }
 
     public function extend($array, $recursive = FALSE) {
 
-        if(! is_array($array))
+        if (!is_array($array))
             return FALSE;
-
+        
         foreach($array as $key => $value) {
-
-            if(is_array($value) && $recursive) {
-
+            
+            if (is_array($value) && $recursive) {
+                
                 $c = $this->get($key);
-
-                if($c && is_array($c))
+                
+                if ($c && is_array($c))
                     $value = array_merge_recursive($c, $value);
-
+                
                 $this->set($key, $value);
-
             } else {
-
+                
                 $this->set($key, $value);
-
             }
-
         }
-
+    
     }
 
     public function clear() {
 
         return $this->backend->clear();
-
+    
     }
 
     public function setValues($values) {
 
-        if(! is_array($values))
+        if (!is_array($values))
             return FALSE;
-
+        
         foreach($values as $key => $value)
             $this->set($key, $value);
-
+        
         return TRUE;
+    
     }
 
     public function pull($key) {
 
         $value = $this->get($key);
-
+        
         $this->remove($key);
-
+        
         return $value;
-
+    
     }
 
     public function toArray() {
 
-        if($this->backend->can('array'))
+        if ($this->backend->can('array'))
             return $this->backend->toArray();
-
+        
         return FALSE;
-
+    
     }
 
     /*
      * MAGIC METHOD FOR DIRECT ACCESS
      */
-
     public function __isset($key) {
 
         return $this->has($key);
-
+    
     }
 
-    public function & __get($key) {
+    public function &__get($key) {
 
         return $this->get($key);
-
+    
     }
 
     public function __set($key, $value) {
 
         return $this->set($key, $value);
-
+    
     }
 
     public function __unset($key) {
 
         return $this->remove($key);
-
+    
     }
 
     /*
@@ -286,25 +290,25 @@ class Cache implements \ArrayAccess {
     public function offsetExists($offset) {
 
         return $this->has($offset);
-
+    
     }
 
     public function offsetGet($offset) {
 
         return $this->get($offset);
-
+    
     }
 
     public function offsetSet($offset, $value) {
 
         return $this->set($offset, $value);
-
+    
     }
 
     public function offsetUnset($offset) {
 
         return $this->remove($offset);
-
+    
     }
 
 }
