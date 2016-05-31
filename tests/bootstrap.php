a466337713932ffdf2fdad6206999449cb300a00
Jamie Carl <jamie@hazaarlabs.com>
2016-05-31 16:33:47 +1000

    Fixed unit tests bootstrap file for updated core module.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,8 +4,8 @@ set_include_path(implode(PATH_SEPARATOR, array(
     getcwd(),
     get_include_path(),
 )));
-require_once ('Hazaar/HelperFunctions.php');
-require_once ('Hazaar/Loader.php');
+require_once ('Hazaar/Core/HelperFunctions.php');
+require_once ('Hazaar/Core/Loader.php');
 $loader = Hazaar\Loader::getInstance();
 $loader->register();
 //Set up the $_SERVER variable with some stuff that we need and can then predict.
