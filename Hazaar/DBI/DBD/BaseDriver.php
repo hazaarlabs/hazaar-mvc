4848ba71626e7d07ac4a9d65ae661e8959df2d99
Jamie Carl <jamie@hazaarlabs.com>
2016-07-08 11:12:22 +1000

    Fixed bad use of keyword 'schema' in DBI::DBD::BaseDriver

--- Hazaar/DBI/DBD/BaseDriver.php
+++ Hazaar/DBI/DBD/BaseDriver.php
@@ -380,7 +380,7 @@ public function prepareCriteria($criteria, $bind_type = 'AND', $tissue = '=', $p
 
         $sql = "
             SELECT
-                table_schema as schema,
+                table_schema as `schema`,
                 table_name as name
             FROM information_schema.tables t
             WHERE table_type = 'BASE TABLE'
