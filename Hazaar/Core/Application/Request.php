8ba6519f8fec97551a9d25a99c23cf7622367f37
Jamie Carl <jamie@hazaarlabs.com>
2016-07-13 14:24:43 +1000

    Added methods to Hazaar\Application\Request to get the request method.
    Can't believe I haven't had this till now.  It's now possible to detect the method, be it GET, POST, PUT, DELETE or some other.

--- Hazaar/Core/Application/Request.php
+++ Hazaar/Core/Application/Request.php
@@ -359,5 +359,35 @@ abstract class Request implements Request\_Interface {
         return $ret;
 
     }
+    
+    public function method(){ 
+ 
+        return $_SERVER['REQUEST_METHOD']; 
+ 
+    } 
+ 
+    public function isGET(){ 
+ 
+        return ($this->method == 'POST'); 
+ 
+    } 
+ 
+    public function isPOST(){ 
+ 
+        return ($this->method == 'POST'); 
+ 
+    } 
+ 
+    public function isPUT(){ 
+ 
+        return ($this->method == 'PUT'); 
+ 
+    } 
+ 
+    public function isDELETE(){ 
+ 
+        return ($this->method == 'DELETE'); 
+ 
+    } 
 
 }
