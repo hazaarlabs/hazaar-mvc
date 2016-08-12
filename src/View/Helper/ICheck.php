64000a51d9060343747984d23614a57f5bc9f5a3
Jamie Carl <jamie@hazaarlabs.com>
2016-08-12 17:16:05 +1000

    Added the iCheck view helper

---
+++ src/View/Helper/ICheck.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @file        Hazaar/View/Helper/iCheck.php
+ *
+ * @author      Jamie Carl <jamie@hazaarlabs.com>
+ *
+ * @copyright   Copyright (c) 2016 Jamie Carl (http://www.hazaarlabs.com)
+ */
+
+namespace Hazaar\View\Helper;
+
+/**
+ * @brief       iCheck view helper
+ *
+ * @detail      For more information on iCheck, see: http://icheck.fronteed.com/
+ *
+ * @since       3.0.0
+ */
+class iCheck extends \Hazaar\View\Helper {
+
+    private $theme = 'minimal';
+
+    private $colour = 'purple';
+
+    public function import() {
+
+        $this->requires('html');
+
+        $this->requires('JQuery');
+
+    }
+
+    /**
+     * @detail      Initialise the view helper and include the buttons.css file.  Adds a requirement for the HTML view
+     *              helper.
+     */
+    public function init($view, $args = array()) {
+
+        $view->link('https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/all.css');
+
+        $view->requires('https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js');
+
+        $this->theme = ake($args, 'theme', 'minimal');
+
+        $this->colour = ake($args, 'colour', 'purple');
+
+    }
+
+    public function post(){
+
+        return $this->html->script("$('input').iCheck({
+            checkboxClass: 'icheckbox_{$this->theme}-{$this->colour}',
+            radioClass: 'iradio_{$this->theme}-{$this->colour}'
+        });");
+
+
+    }
+
+}
