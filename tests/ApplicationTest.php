7c3fb5861c31a29c8870fd814e5d73285ffde995
Jamie Carl <jamie@hazaarlabs.com>
2016-11-23 19:08:44 +1100

    Updated tests for JSON configs.

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -10,17 +10,7 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase {
 
     protected function setUp() {
 
-        $this->config_file = realpath(APPLICATION_PATH . '/configs/application.ini');
-
-        $this->app = new \Hazaar\Application('development', $this->config_file);
-
-    }
-
-    public function testCanFindConfig() {
-
-        $this->assertNotNull($this->config_file);
-
-        $this->assertTrue(file_exists($this->config_file));
+        $this->app = new \Hazaar\Application('development');
 
     }
 
@@ -35,19 +25,19 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase {
     public function testReadConfig() {
 
         $this->assertInstanceOf('Hazaar\Application\Config', $this->app->config);
-        
+
         $this->assertEquals('Example Application', $this->app->config->app->name);
-        
+
         $this->assertEquals('0.0.1', $this->app->config->app->version);
 
         $this->assertEquals(1, $this->app->config->app->debug);
 
     }
-    
+
     public function testLoadIndex(){
-        
+
         $this->assertInstanceOf('Hazaar\Application', $this->app->bootstrap());
-        
+
     }
 
 }
