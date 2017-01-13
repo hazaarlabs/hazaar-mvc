507bf8b6845163717a5040861799f6c52dbe2c0b
Jamie Carl <jamie@hazaarlabs.com>
2017-01-13 13:43:54 +1100

    Auth and schema man updates
    * Authentication is now a thing.  Uses .htpasswd format (SHA only)
    * Schame manager now has snapshot and migrate basic function

--- libs/admin/application.js
+++ libs/admin/application.js
@@ -1,4 +1,28 @@
-﻿$(document).ready(function () {
-
-
+﻿function showLog(obj, result) {
+    var panel = obj.parent().removeClass('panel-default');
+    if (result.ok)
+        panel.addClass('panel-success');
+    else
+        panel.addClass('panel-danger');
+    for (x in result.log) {
+        var thetime = new Date(result.log[x].time * 1000);
+        obj.append($('<div>').append($('<div class="log-time">').html(thetime.toLocaleString()), $('<div class="log-msg">').html(result.log[x].msg)));
+    }
+}
+$(document).ready(function () {
+    $('#frmSnapshot').submit(function () {
+        var data = $(this).serializeArray();
+        $('#snapshotlog').empty().parent().addClass('panel-default').removeClass('panel-success').removeClass('panel-danger');
+        $.post(hazaar.url('hazaar', 'snapshot'), data).done(function (result) {
+            showLog($('#snapshotlog'), result);
+        });
+        return false;
+    });
+    $('#frmMigrate').submit(function () {
+        var data = $(this).serializeArray();
+        $.post(hazaar.url('hazaar', 'migrate'), data).done(function (result) {
+            showLog($('#migratelog'), result);
+        });
+        return false;
+    });
 });
--- src/Controller/Hazaar.php
+++ src/Controller/Hazaar.php
@@ -4,6 +4,217 @@ namespace Hazaar\Controller;
 
 class Hazaar extends \Hazaar\Controller\Action {
 
+    private $open_actions = array('file', 'login');
+
+    public function init(){
+
+        if(in_array($this->request->getActionName(), $this->open_actions))
+            return;
+
+        session_start();
+
+        $this->view->addHelper('bootstrap');
+
+        if(!ake($_SESSION, 'user'))
+            $this->redirect($this->url('login'));
+
+        $this->layout('@admin/layout');
+
+        $this->view->addHelper('jQuery');
+
+        $this->view->addHelper('fontawesome', array('version' => '4.7.0'));
+
+        $this->view->requires($this->application->url('hazaar/file/admin/application.js'));
+
+        $this->view->link($this->application->url('hazaar/file/admin/layout.css'));
+
+        $this->view->navitems = array(
+            'app' => array(
+                'label' => 'Application',
+                'items' => array(
+                    'index' => 'Overview',
+                    'models' => 'Models',
+                    'views' => 'Views',
+                    'controllers' => 'Controllers'
+                )
+            )
+        );
+
+        if(class_exists('Hazaar\DBI\Adapter')){
+
+            $this->view->navitems['cache'] = array(
+                'label' => 'Cache',
+                'items' => array(
+                    'settings' => 'Settings'
+                )
+            );
+
+        }
+
+        if(class_exists('Hazaar\DBI\Adapter')){
+
+            $this->view->navitems['db'] = array(
+                'label' => 'Database',
+                'items' => array(
+                    'settings' => 'Settings',
+                    'schema' => 'Schema Managment'
+                )
+            );
+
+            if($this->action == 'db_schema'){
+
+                $db = new \Hazaar\DBI\Adapter();
+
+                $this->view->versions = array('latest' => 'Latest Version') + $db->getSchemaVersions();
+
+            }
+
+        }
+
+        if(class_exists('Hazaar\Warlock\Control')){
+
+            $this->view->navitems['warlock'] = array(
+                'label' => 'Warlock',
+                'items' => array(
+                    'index' => 'Overview',
+                    'connections' => 'Connections',
+                    'processes' => 'Processes',
+                    'services' => 'Services',
+                    'log' => 'Log File'
+                )
+            );
+
+        }
+
+    }
+
+
+    public function login(){
+
+        session_start();
+
+        if($this->request->isPOST()){
+
+            $passwd = CONFIG_PATH . DIRECTORY_SEPARATOR . '.passwd';
+
+            if(!file_exists($passwd))
+                die('Hazaar admin console is currently disabled!');
+
+            $users = array();
+
+            $lines = explode("\n", trim(file_get_contents($passwd)));
+
+            foreach($lines as $line){
+
+                if(!$line)
+                    continue;
+
+                list($identity, $userhash) = explode(':', $line);
+
+                $users[$identity] = $userhash;
+
+            }
+
+            $credential = trim(ake($users, $this->request->username));
+
+            if(strlen($credential) > 0){
+
+                $hash = '';
+
+                if(substr($credential, 0, 6) == '$apr1$'){
+
+                    throw new \Exception('APR1-MD5 encoded passwords are not supported!');
+
+                }elseif(substr($credential, 0, 5) == '{SHA}'){
+
+                    $hash = '{SHA}' . base64_encode(sha1($this->request->password, TRUE));
+
+                }
+
+                if($hash == $credential){
+
+                    $_SESSION['user'] = $this->request->username;
+
+                    $this->redirect($this->url());
+
+                }else{
+
+                    $this->view->msg = 'Password is incorrect!';
+
+                }
+
+            }else{
+
+                $this->view->msg = 'User is not found!';
+
+            }
+
+        }elseif(ake($_SESSION, 'user')){
+
+            $this->redirect($this->url());
+
+        }
+
+        $this->layout('@admin/login');
+
+        $this->view->addHelper('bootstrap');
+
+        $this->view->link($this->application->url('hazaar/file/admin/login.css'));
+
+    }
+
+    public function logout(){
+
+        session_unset();
+
+        $this->redirect($this->url());
+
+    }
+
+
+    /**
+     * Launch the Hazaar MVC Management Console
+     *
+     * The Management Console is a virtual desktop that allows the application to be
+     * administered in a user friendly environment.
+     */
+    public function __default($controller, $action){
+
+        $this->view('@admin/' . str_replace('_', '/', $action));
+
+    }
+
+    public function snapshot(){
+
+        if(!$this->request->isPOST())
+            return false;
+
+        $db = new \Hazaar\DBI\Adapter();
+
+        $result = $db->snapshot($this->request->get('comment'), boolify($this->request->get('testmode', false)));
+
+        return array('ok' => $result, 'log' => $db->getMigrationLog());
+
+    }
+
+    public function migrate(){
+
+        if(!$this->request->isPOST())
+            return false;
+
+        $version = $this->request->get('version', 'latest');
+
+        if($version == 'latest')
+            $version = null;
+
+        $db = new \Hazaar\DBI\Adapter();
+
+        $result = $db->migrate($version, boolify($this->request->get('testmode', false)));
+
+        return array('ok' => $result, 'log' => $db->getMigrationLog());
+
+    }
+
     /**
      * Directly access a file stored in the Hazaar libs directory.
      *
@@ -43,104 +254,4 @@ class Hazaar extends \Hazaar\Controller\Action {
         return $response;
 
     }
-
-    /**
-     * Launch the Hazaar MVC Management Console
-     *
-     * The Management Console is a virtual desktop that allows the application to be
-     * administered in a user friendly environment.
-     */
-    public function __default($controller, $action){
-
-        session_start();
-
-        $this->view->addHelper('bootstrap');
-
-        if($this->request->getActionName() == 'logout'){
-
-            session_unset();
-
-            $this->redirect($this->url());
-
-        }elseif(ake($_SESSION, 'user')){
-
-            $this->layout('@admin/layout');
-
-            $this->view->addHelper('jQuery');
-
-            $this->view->addHelper('fontawesome', array('version' => '4.7.0'));
-
-            $this->view->requires($this->application->url('hazaar/file/admin/application.js'));
-
-            $this->view->link($this->application->url('hazaar/file/admin/layout.css'));
-
-            $this->view('@admin/' . str_replace('_', '/', $action));
-
-            $this->view->navitems = array(
-                'app' => array(
-                    'label' => 'Application',
-                    'items' => array(
-                        'index' => 'Overview',
-                        'models' => 'Models',
-                        'views' => 'Views',
-                        'controllers' => 'Controllers'
-                    )
-                )
-            );
-
-            if(class_exists('Hazaar\DBI\Adapter')){
-
-                $this->view->navitems['cache'] = array(
-                    'label' => 'Cache',
-                    'items' => array(
-                        'settings' => 'Settings'
-                    )
-                );
-
-            }
-
-            if(class_exists('Hazaar\DBI\Adapter')){
-
-                $this->view->navitems['db'] = array(
-                    'label' => 'Database',
-                    'items' => array(
-                        'settings' => 'Settings',
-                        'schema' => 'Schema Managment'
-                    )
-                );
-
-            }
-
-            if(class_exists('Hazaar\Warlock\Control')){
-
-                $this->view->navitems['warlock'] = array(
-                    'label' => 'Warlock',
-                    'items' => array(
-                        'index' => 'Overview',
-                        'connections' => 'Connections',
-                        'processes' => 'Processes',
-                        'services' => 'Services',
-                        'log' => 'Log File'
-                    )
-                );
-
-            }
-
-        }elseif($this->request->getActionName() !== 'login'){
-
-            if($action == 'authenticate'){
-
-                if($_SESSION['user'] = $this->request->username)
-                    $this->redirect($this->url());
-
-            }
-
-            $this->layout('@admin/login');
-
-            $this->view->link($this->application->url('hazaar/file/admin/login.css'));
-
-        }
-
-    }
-
 }
