5e6af317e035307e85ffb40ffd23f71ac35d3519
Jamie Carl <jamie@hazaarlabs.com>
2017-03-30 00:39:41 +1100

    Console: Started adding config management
    refs #12
    refs #46

---
+++ libs/console/js/configs.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $.get(hazaar.url('app', 'configs')).done(function (data) {
+        var binder = new dataBinder({ files: data });
+        var tbody = $('#configFiles').children('tbody');
+        for (x in binder.files) {
+            var file = binder.files[x];
+            var action = $('<button>').data('i', x);
+            if (file.encrypted) {
+                action.html('Decrypt').addClass('danger');
+            } else {
+                action.html('Encrypt').addClass('success');
+            }
+            tbody.append($('<tr>').html([
+                $('<td data-bind="files[' + x + '].name">').html(file.name),
+                $('<td data-bind="files[' + x + '].size">').html(file.size),
+                $('<td data-bind="files[' + x + '].encrypted">').html((file.encrypted ? 'Yes' : 'No')),
+                $('<td>').html(action)
+            ]).data('file', file));
+        }
+        $('#configFiles').find('button').click(function (event) {
+            var i = $(this).data('i');
+            binder.files[i].encrypted = true;
+        });
+    });
+});
--- src/Console/Application.php
+++ src/Console/Application.php
@@ -15,6 +15,7 @@ class Application extends Module {
         $this->addMenuItem('app', 'Controllers', 'controllers', 'code-fork', 5);
 
         $this->addMenuGroup('sys', 'System', 'wrench');
+        $this->addMenuItem('app', 'Configuration', 'configs', 'cogs');
 
         $this->addMenuItem('sys', 'PHP Info', 'phpinfo');
 
@@ -43,6 +44,59 @@ class Application extends Module {
     public function controllers($request){
 
         $this->view('controllers');
+
+    }
+
+    public function configs($request){
+
+        if($request->isXMLHttpRequest()){
+
+            $files = array();
+
+            $search_paths = $this->application->loader->getSearchPaths(FILE_PATH_CONFIG);
+
+            foreach($search_paths as $path){
+
+                $dir = new \Hazaar\File\Dir($path);
+
+                $config_files = $dir->find('*.json');
+
+                foreach($config_files as $config_file){
+
+                    $file = new \Hazaar\File($config_file);
+
+                    $r = $file->open();
+
+                    $bom = pack('H*','BADA55');  //Haha, Bad Ass!
+
+                    $encrypted = (fread($r, 3) == $bom);
+
+                    $file->close();
+
+                    $files[] = array(
+                        'name' => trim(str_replace($path, '', $file->fullpath()), DIRECTORY_SEPARATOR),
+                        'size' => $file->size(),
+                        'encrypted' => $encrypted
+                    );
+
+                }
+
+            }
+
+            return $files;
+
+        }
+
+        $this->view('configs');
+
+        $this->view->requires('js/configs.js');
+
+    }
+
+    public function system($request){
+
+        $this->view('system');
+
 
     }
 
