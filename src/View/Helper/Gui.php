b79fb3eeeba1c3b2b2d3d43de607811e90b27f3d
Jamie Carl <jamie@hazaarlabs.com>
2017-08-29 14:42:04 +1000

    Massive tidy up.
    * JqWidgets is gone.  They can get stuffed all that work I did for their HTML editor and they "can't use it due to licence agreements".  Fuck them.
    * Integrated my popup library.

---
+++ src/View/Helper/Gui.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @file        Hazaar/View/Helper/Gui.php
+ *
+ * @author      Jamie Carl <jamie@hazaarlabs.com>
+ *
+ * @copyright   Copyright (c) 2012 Jamie Carl (http://www.hazaarlabs.com)
+ */
+
+namespace Hazaar\View\Helper;
+
+/**
+ * @brief       Hazaar Built-in GUI Tools
+ *
+ * @since       2.3.12
+ */
+class Gui extends \Hazaar\View\Helper {
+
+    private $parser;
+
+    public function import(){
+
+        $this->requires('jQuery');
+
+    }
+
+    public function init($view, $args = array()) {
+
+        $view->requires($this->application->url('hazaar', 'file/js/popup.js'));
+
+        $view->link($this->application->url('hazaar', 'file/css/popup.css'));
+
+    }
+
+    public function popup($content, $args = array()) {
+
+        $id = 'popup_' . uniqid();
+
+        $this->jquery->exec("$('#$id').popup();");
+
+        return $this->html->div($content)->id($id);
+
+    }
+
+}
+
