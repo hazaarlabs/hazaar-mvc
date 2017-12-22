68ca64c817c53d257b42562fb58fad2fc8291ade
Jamie Carl <jamie@hazaarlabs.com>
2017-12-22 12:19:03 +1100

    Models: Added array_walk_recursive

--- src/Model/ChildArray.php
+++ src/Model/ChildArray.php
@@ -67,6 +67,35 @@ class ChildArray extends DataTypeConverter implements \ArrayAccess, \Iterator, \
     }
 
     /**
+     * Apply a user supplied function to every member of an array
+     *
+     * Applies the user-defined callback function to each element of the array array.
+     *
+     * ChildArray::walk() is not affected by the internal array pointer of array. ChildArray::walk() will
+     * walk through the entire array regardless of pointer position.
+     *
+     * For more information on this method see PHP's array_walk() function.
+     *
+     * @param mixed $callback   Typically, callback takes on two parameters. The array parameter's value being
+     *                          the first, and the key/index second.
+     * @param mixed $userdata   If the optional userdata parameter is supplied, it will be passed as the third
+     *                          parameter to the callback.
+     */
+    public function array_walk_recursive($callback, $userdata = NULL){
+
+        foreach($this->values as $key => &$value){
+
+            if($value instanceof Strict || $value instanceof ChildArray)
+                $value->array_walk_recursive($callback, $userdata);
+            else
+                $callback($value, $key, $userdata);
+
+        }
+
+    }
+
+
+    /**
      * Magic method for calling array_* functions on the ChildArray class.
      *
      * @param mixed $func
