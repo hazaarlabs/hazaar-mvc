31492d2cf61952d1fc7d2bf38bdb18a39e1ca3f8
Jamie Carl <jamie@hazaarlabs.com>
2019-01-01 16:51:16 +1100

    Added the first pass at application metrics.
    Basic view with 4 graphs available on the one page.

--- libs/console/js/application.js
+++ libs/console/js/application.js
@@ -1,8 +1,2 @@
 ﻿$(document).ready(function () {
-    $.post(hazaar.url('app', 'stats'), {
-        name: 'hits',
-        archive: 'avg_min_1hour'
-    }).done(function (data) {
-        console.log(data.ticks);
-    });
 });
