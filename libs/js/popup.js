f7a4fdfe564530a8214d8ab33ecaf8d33d020f25
Jamie Carl <jamie@hazaarlabs.com>
2019-01-01 15:21:40 +1100

    Console: Updated popup JS/CSS

--- libs/js/popup.js
+++ libs/js/popup.js
@@ -1,4 +1,4 @@
-﻿$.fn.popup = function () {
+$.fn.popup = function () {
     var args = arguments;
     this.each(function (index, host) {
         if (!host.show) {
@@ -7,7 +7,8 @@
                 buttons: [],
                 icon: null,
                 modal: false,
-                hideOnClose: false
+                hideOnClose: false,
+                soft: true
             }, args[0]);
             host.center = function () {
                 var x = (window.innerWidth / 2) - (this.__window.width() / 2), y = (window.innerHeight / 2) - (this.__window.height() / 2);
@@ -25,26 +26,22 @@
                 if (!$(host).is(':visible')) $(host).show();
                 if (this.__overlay) {
                     this.__window.show();
-                    this.__overlay.fadeIn();
+                    this.__overlay.fadeIn(function () { $(host).trigger('open'); });
                 } else {
-                    this.__window.fadeIn();
+                    this.__window.fadeIn(function () { $(host).trigger('open'); });
                 }
                 this.center();
             };
             host.btnAction = function (event) {
                 var btn = $(this).data('btn');
-                if (typeof btn.action == 'function')
-                    btn.action.apply(host, event);
-                else if (btn.action == 'close')
-                    host.close();
-                else if (btn.action == 'post' && btn.target) {
+                if (btn.action === 'post' && btn.target) {
                     var data = {}, items = host.__window.find('input,select,textarea').serializeArray();
                     if (btn['data-source']) {
                         $(btn['data-source']).find('input[name],select[name],textarea[name]').each(function (index, item) {
                             var value = item.value;
-                            if (item.type == 'checkbox')
+                            if (item.type === 'checkbox')
                                 value = $(item).is(':checked');
-                            else if ($(item).attr('aria-hidden') == 'true' && $(item).prev().hasClass('mce-tinymce'))
+                            else if ($(item).attr('aria-hidden') === 'true' && $(item).prev().hasClass('mce-tinymce'))
                                 value = tinymce.get('content').getContent();
                             data[item.name] = value;
                         });
@@ -58,84 +55,80 @@
                         post.error(eval(btn.error));
                     if (btn.done)
                         post.done(eval(btn.done));
-                    host.close();
+                } else {
+                    if (typeof btn.action === 'string')
+                        btn.action = new Function(btn.action);
+                    if (typeof btn.action === 'function')
+                        btn.action.apply(host, event);
                 }
+                host.close();
             };
             host.__setIcon = function (name, color) {
-                var icon_class = 'font-awesome';
+                var icon = 'font-awesome';
+                if (typeof color === 'undefined') color = '#000';
                 if (!this.__icon)
-                    this.__icon = $('<div class="hazaar modal-content-icon">').appendTo(this.__container);
-                var icons = {
-                    "working": {
-                        "icon": "circle-o-notch fa-spin",
-                        "color": "#333"
-                    },
-                    "error": {
-                        "icon": "times-circle",
-                        "color": "#cf3838"
-                    },
-                    "danger": {
-                        "icon": "times-circle",
-                        "color": "#cf3838"
-                    },
-                    "info": {
-                        "icon": "info-circle",
-                        "color": "#3A85CF"
-                    },
-                    "notice": {
-                        "icon": "info-circle",
-                        "color": "#3A85CF"
-                    },
-                    "warn": {
-                        "icon": "exclamation-circle",
-                        "color": "#FF9900"
-                    },
-                    "warning": {
-                        "icon": "exclamation-circle",
-                        "color": "#FF9900",
-                    },
-                    "question": {
-                        "icon": "question-circle",
-                        "color": "#3A85CF"
-                    },
-                    "user": {
-                        "icon": "user-circle",
-                        "color": "#00B36B"
-                    },
-                    "success": {
-                        "icon": "check-circle",
-                        "color": "#00B36B"
-                    }
-                };
-                var icon_class = 'fa-font-awesome';
-                if (name in icons) {
-                    icon_class = icons[name].icon;
-                    if (!color) color = icons[name].color;
-                } else icon_class = name;
-                this.__icon.html($('<i class="fa fa-' + icon_class + '">'));
-                if (color) this.__icon.css({ color: color });
+                    this.__icon = $('<div class="popup-window-icon">').appendTo(this.__container);
+                switch (name) {
+                    case 'working':
+                        icon = 'circle-o-notch fa-spin';
+                        color = '#333';
+                        break;
+                    case 'error':
+                    case 'danger':
+                        icon = 'times-circle';
+                        color = '#cf3838';
+                        break;
+                    case 'info':
+                    case 'notice':
+                        icon = 'info-circle';
+                        color = '#3A85CF';
+                        break;
+                    case 'warn':
+                    case 'warning':
+                        icon = 'exclamation-circle';
+                        color = '#FF9900';
+                        break;
+                    case 'question':
+                        icon = 'question-circle';
+                        color = '#3A85CF';
+                        break;
+                    case 'user':
+                        icon = 'user-circle';
+                        color = '#00B36B';
+                        break;
+                    case 'success':
+                        icon = 'check-circle';
+                        color = '#00B36B';
+                        break;
+                }
+                this.__icon.html($('<i class="fa fa-' + icon + '">'));
+                if (color)
+                    this.__icon.css({ color: color });
             };
             host.render = function () {
-                this.__overlay = $('<div class="hazaar popup-overlay">').appendTo(document.body).toggleClass('modal', this.props.modal);
-                this.__window = $('<div class="hazaar modal-content">').appendTo(this.__overlay);
-                this.__title = $('<div class="hazaar modal-header">').html(this.props.title).appendTo(this.__window);
-                this.__container = $('<div class="hazaar modal-body">').appendTo(this.__window);
-                this.__close = $('<button type="button" class="close">').html('x').appendTo(this.__title);
+                this.__overlay = $('<div class="popup-overlay">').appendTo(document.body).toggleClass('modal', this.props.modal);
+                this.__window = $('<div class="popup-window">').appendTo(this.__overlay);
+                this.__title = $('<div class="popup-window-title">').html(this.props.title).appendTo(this.__window);
+                this.__container = $('<div class="popup-container">').appendTo(this.__window);
+                this.__close = $('<div class="popup-window-close">').html('X').appendTo(this.__title);
                 if (this.props.icon)
                     this.__setIcon(this.props.icon, this.props.iconColor);
                 this.props.hideOnClose = ($(this).parent().length > 0);
                 this.__container.append(this);
                 if (this.props.buttons.length > 0) {
-                    this.__buttons = $('<div class="hazaar modal-footer">').appendTo(this.__window);
+                    this.__buttons = $('<div class="popup-buttons">').appendTo(this.__window);
                     for (x in this.props.buttons) {
-                        if (typeof this.props.buttons[x] == 'string')
+                        if (typeof this.props.buttons[x] === 'string')
                             this.props.buttons[x] = { label: this.props.buttons[x], action: 'close' };
-                        var btn = $.extend({ label: 'Button', action: 'close', 'class': 'popup-button' }, this.props.buttons[x]);
-                        var button = $('<button>').attr('class', btn.class).html(btn.label).data('btn', btn).click(this.btnAction);
-                        this.__buttons.append(button);
+                        var args = $.extend({ label: 'Button', action: 'close' }, this.props.buttons[x]);
+                        var btn = $('<button>').html(args.label).data('btn', args).click(this.btnAction);
+                        if (!('class' in args)) args.class = 'btn btn-default';
+                        this.__buttons.append(btn.addClass(args.class));
                     }
                 }
-                //$(this).addClass('modal-body');
+                $(this).addClass('popup-content');
+                if (this.props.id)
+                    $(this).attr('id', this.props.id);
                 if (this.props.width)
                     this.__window.addClass('static').css({ width: this.props.width });
                 if (this.props.height)
@@ -144,6 +137,10 @@
                     this.__window.css({ 'max-width': this.props['max-width'] });
                 if (this.props['max-height'])
                     this.__window.css({ 'max-height': this.props['max-height'] });
+                if (this.props.soft)
+                    this.__container.addClass('soft');
+                if (this.props.zindex)
+                    this.__overlay.css('z-index', this.props.zindex);
             };
             host.__move = function () {
                 host.__window.css({ left: event.pageX - host.__offset[0], top: event.pageY - host.__offset[1] });
@@ -170,7 +167,7 @@
                     host.close();
                 }, host.props.close);
             }
-        } else if (typeof args[0] == 'string') {
+        } else if (typeof args[0] === 'string') {
             switch (args[0]) {
                 case 'close':
                     this.close();
