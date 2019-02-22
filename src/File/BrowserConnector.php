d39ab7ee085853fa9c40d72180cdde2d57744ee8
Jamie Carl <jamie@hazaarlabs.com>
2019-02-22 15:52:57 +1100

    File browser find is now always wildcard

--- src/File/BrowserConnector.php
+++ src/File/BrowserConnector.php
@@ -622,7 +622,7 @@ class BrowserConnector {
 
         $path = $this->path($target);
 
-        $list = $source->find('*' . $query . '*', $path, true);
+        $list = $source->find($query, $path, true);
 
         if(!is_array($list))
             throw new \Exception('Search failed!');
