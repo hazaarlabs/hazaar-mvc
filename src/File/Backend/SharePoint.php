2fb1062043f10971ee99e4c9e55218c199c7d29f
Jamie Carl <jamie@hazaarlabs.com>
2019-10-10 16:15:14 +1100

    SharePoint initial work
    I have some absolute basics under way.  Such as:
    
    * Authentication, which is working.
    * Scanning a folder, which is 50% done.  We can get the query from SP and now just need to process the response.

--- src/File/Backend/SharePoint.php
+++ src/File/Backend/SharePoint.php
@@ -2,26 +2,57 @@
 
 namespace Hazaar\File\Backend;
 
+use \Hazaar\Http\Request;
+
 class SharePoint extends \Hazaar\Http\Client implements _Interface {
 
     public  $separator  = '/';
 
     private $options;
 
+    private $cache;
+
+    private static $STSAuthURL = 'https://login.microsoftonline.com/extSTS.srf';
+
+    private static $signInURL = '/_forms/default.aspx?wa=wsignin1.0';
+
+    private $auth_cookies = null;
+
     static public function label(){
 
         return "Microsoft SharePoint";
 
     }
-    
+
     public function __construct($options) {
 
         parent::__construct();
 
-        $this->options = new \Hazaar\Map(array('webURL'   => '', 'username' => '', 'password' => ''), $options);
+        $this->disableRedirect();
 
-        if(! ($this->options->isEmpty('webURL') && $this->options->isEmpty('username') && $this->options->isEmpty('password')))
+        $this->options = new \Hazaar\Map(array(
+            'webURL'        => '',
+            'username'      => '',
+            'password'      => '',
+            'root'          => '/',
+            'cache_backend' => 'file'
+        ), $options);
+
+        if($this->options->isEmpty('webURL') || $this->options->isEmpty('username') || $this->options->isEmpty('password'))
             throw new Exception\DropboxError('SharePoint filesystem backend requires a webURL, username and password.');
+
+        $cache_options = array(
+            'use_pragma' => FALSE,
+            'namespace' => 'sharepoint_' . md5($this->options->username . ':' . $this->options->password . '@' . $this->options->webURL)
+        );
+
+        $this->cache = new \Hazaar\Cache($this->options['cache_backend'], $cache_options);
+
+        $this->uncacheCookie($this->cache);
+
+    }
+
+    public function __destruct() {
 
     }
 
@@ -33,19 +64,101 @@ class SharePoint extends \Hazaar\Http\Client implements _Interface {
 
     }
 
-    public function __destruct() {
+    public function authorise($redirect_uri = NULL) {
+
+        if($this->authorised())
+            return true;
+
+        if(!($token = $this->getSecurityToken($this->options['username'], $this->options['password'])))
+            throw new \Exception('Unable to get SharePoint security token!');
+
+        return $this->getAuthenticationCookies($token);
 
     }
 
-    public function authorise($redirect_uri = NULL) {
+    private function getSecurityToken($username, $password){
+
+        $xmlFile = __DIR__ . DIRECTORY_SEPARATOR . 'XML' . DIRECTORY_SEPARATOR . 'SAML.xml';
+
+        if(!file_exists($xmlFile))
+            throw new \Exception('SAML XML authorisation template is missing!');
+
+        $request = new Request(self::$STSAuthURL, 'POST');
+
+        $request->setHeader('Accept', 'application/json; odata=verbose');
+
+        $template = file_get_contents($xmlFile);
+
+        $template = str_replace('{username}', $this->options['username'], $template);
+
+        $template = str_replace('{password}', $this->options['password'], $template);
+
+        $template = str_replace('{address}', $this->options['webURL'], $template);
+
+        $request->setBody($template);
+
+        $response = $this->send($request);
+
+        if($response->status !== 200)
+            throw new \Exception('Invalid response requesting security token.');
+
+        $xml = new \DOMDocument();
+
+        $xml->loadXML($response->body());
+
+        if(!$xml instanceof \DOMDocument)
+            throw new \Exception('Invalid response authenticating SharePoint access.');
+
+        $xpath = new \DOMXPath($xml);
+
+        if ($xpath->query("//wsse:BinarySecurityToken")->length > 0){
+
+            $nodeToken = $xpath->query("//wsse:BinarySecurityToken")->item(0);
+
+            if (!empty($nodeToken))
+                return $nodeToken->nodeValue;
+
+        }
+
+        if ($xpath->query("//S:Fault")->length > 0)
+            throw new \RuntimeException($xpath->query("//S:Fault")->item(0)->nodeValue);
 
         return false;
+
+    }
+
+    private function getAuthenticationCookies($token){
+
+        $url_info = parse_url($this->options['webURL']);
+
+        $url =  $url_info['scheme'] . '://' . $url_info['host'] . self::$signInURL;
+
+        $request = new Request($url, 'POST');
+
+        $request->setBody($token);
+
+        $response = $this->send($request, false);
+
+        if($response->status !== 302)
+            throw new \Exception('Invalid response requesting auth cookies: ' . $response->status);
+
+        $this->deleteCookie('fpc');
+
+        $this->deleteCookie('x-ms-gateway-slice');
+
+        $this->deleteCookie('stsservicecookie');
+
+        $this->deleteCookie('RpsContextCookie');
+
+        $this->cacheCookie($this->cache);
+
+        return true;
 
     }
 
     public function authorised() {
 
-        return false;
+        return ($this->hasCookie('FedAuth') && $this->hasCookie('rtFa'));
 
     }
 
@@ -55,6 +168,31 @@ class SharePoint extends \Hazaar\Http\Client implements _Interface {
 
     //Get a directory listing
     public function scandir($path, $regex_filter = NULL, $show_hidden = FALSE) {
+
+        $url = $this->options['webURL'] . '/_api/Web'  . $this->makeODataPath($this->options['root'] . '/' . ltrim($path, '/')) . '/files';
+
+        $request = new Request($url, 'GET', 'application/json; OData=verbose');
+
+        $request->setHeader('Accept', 'application/json; OData=verbose');
+
+        $this->applyCookies($request);
+
+        $response = $this->send($request);
+
+        dump($response->body());
+
+    }
+
+    private function makeODataPath($raw_path){
+
+        $path = '';
+
+        $parts = explode('/', trim($raw_path, ' /'));
+
+        foreach($parts as $part)
+            $path .= "/folders/getbyurl('" . rawurlencode($part) . "')";
+
+        return $path;
 
     }
 
