fdf27e13b83987557709ec9d96b5b963332d9ee5
Jamie Carl <jamie@hazaarlabs.com>
2019-10-24 17:25:21 +1100

    Initial WebDAV support is working!
    I have directory listings working.  GETs work using the existing media code, which is cool.  This new controller adds support for OPTIONS and PROPFIND.
    
    Tested against Windows 10 network locations and I can traverse and open files!  Awesome!  Next is COPY, MOVE and DELETE.

--- src/Controller/WebDAV.php
+++ src/Controller/WebDAV.php
@@ -3,7 +3,11 @@
 namespace Hazaar\Controller;
 
 use \Hazaar\Controller\Response;
+use \Hazaar\Controller\Response\Xml;
 use \Hazaar\Controller\Response\HTTP\OK;
+
+use \Hazaar\File;
+use \Hazaar\File\Dir;
 
 abstract class WebDAV extends Basic {
 
@@ -24,12 +28,29 @@ abstract class WebDAV extends Basic {
         'MOVE'
     );
 
-    public function __run(){
+    protected $manager;
+
+    public function __initialize(\Hazaar\Application\Request $request){
+
+        parent::__initialize($request);
+
+        $this->manager = \Hazaar\File\Manager::select($this->__action);
+
+    }
+
+    public function __runAction(&$action = null){
 
         $method = strtolower($this->request->method());
 
-        if(!(in_array(strtoupper($method), $this->__allowed_methods) && method_exists($this, $method)))
-            throw new \Exception('Method not supported', 405);
+        //If the method is not supported, check for a __default handler to pass it off to, or else 405.
+        if(!(in_array(strtoupper($method), $this->__allowed_methods) && method_exists($this, $method))){
+
+            if(!method_exists($this, '__default'))
+                throw new \Exception('Method not supported', 405);
+
+            return $this->__default($this->name, $this->__action);
+
+        }
 
         $response = call_user_func(array($this, $method));
 
@@ -61,9 +82,85 @@ abstract class WebDAV extends Basic {
 
     public function propfind(){
 
-        $path = $this->request->getPath();
+        $path = '/'. ltrim($this->request->getPath(), '/');
 
+        $depth = $this->request->hasHeader('Depth') ? $this->request->getHeader('Depth') : 'infinity';
 
+        if(strtolower($depth) === 'infinity')
+            throw new \Exception('PROPFIND requests with a Depth of "infinity" are not allowed for ' . $path . '.');
+
+        $depth = intval($depth);
+
+        if($depth !== 0 && $depth !== 1)
+            throw new \Exception('Bad request', 400);
+
+        date_default_timezone_set('UTC');
+
+        $object = $this->manager->get($path);
+
+        $xml = new \Hazaar\Xml\Element();
+
+        $xml->addNamespace('D', 'DAV:');
+
+        $ms = $xml->add('D:multistatus');
+
+        $this->writeObjectStatusResponse($object, $ms);
+
+        if($depth === 1 && $object instanceof \Hazaar\File\Dir){
+
+            while($child = $object->read())
+                $this->writeObjectStatusResponse($child, $ms);
+
+        }
+
+        $response = new Xml($xml);
+
+        $response->setStatus(207);
+
+        return $response;
+
+    }
+
+    private function writeObjectStatusResponse($object, \Hazaar\Xml\Element $xml){
+
+        $response = $xml->add('D:response');
+
+        $response->addNamespace('lp1', 'DAV:');
+
+        $response->addNamespace('lp2', 'http://apache.org/dav/props/');
+
+        $response->add('D:href', $object->fullpath());
+
+        $propstat = $response->add('D:propstat');
+
+        $prop = $propstat->add('D:prop');
+
+        $type = $prop->add('lp1:resourcetype');
+
+        if($object instanceof Dir)
+            $type->add('D:collection');
+
+        $prop->add('lp1:creationdate', date('c', $object->ctime()));
+
+        $prop->add('lp1:getlastmodified', date('c', $object->mtime()));
+
+        if($object instanceof File){
+
+            $prop->add('lp1:getcontentlength', $object->size());
+
+            $prop->add('lp2:executable', 'F');
+
+        }
+
+        $prop->add('lp1:getetag', '14-5959eb7f60805');
+
+        $prop->add('D:supportedlock');
+
+        $prop->add('D:getcontenttype', $object->mime_content_type());
+
+        $propstat->add('D:status', ($object->exists() ? 'HTTP/1.1 200 OK' : 'HTTP/1.1 404 Not Found'));
+
+        return $response;
 
     }
 
