e1355780b25b15ba2a38dfc9d6330f37cd036ffd
Jamie Carl <jamie@hazaarlabs.com>
2019-11-15 14:53:01 +1100

    Added quick filesystem unit test

--- tests/FilesystemTest.php
+++ tests/FilesystemTest.php
@@ -6,6 +6,13 @@ class FilesystemTest extends \PHPUnit\Framework\TestCase {
 
     public function testCanDoAdd() {
 
+        $file = new \Hazaar\File\Temp();
+
+        $this->assertTrue($file->exists());
+
+        $this->assertTrue($file->touch());
+
+
     }
 
 }
