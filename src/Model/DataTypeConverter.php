d0df2499eff534a0775184a446597c0d45e70636
Jamie Carl <jamie@hazaar.io>
2021-08-13 12:29:27 +1000

    Strict model data type conversion filters empty values from arrays

--- src/Model/DataTypeConverter.php
+++ src/Model/DataTypeConverter.php
@@ -133,6 +133,13 @@ abstract class DataTypeConverter  {
 
                     $value = $o;
 
+                }elseif(is_array($value)){
+
+                    $value = array_filter($value, function($item){ return !empty($item); });
+
+                    if(count($value) === 0) 
+                        $value = null;
+                        
                 }
 
             }
