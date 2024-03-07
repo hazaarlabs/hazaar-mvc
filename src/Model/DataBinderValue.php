c16ea1eb2a462cc2521463b99527686604fe0c43
Jamie Carl <jamie@hazaar.io>
2024-03-08 10:32:37 +1100

    Revert "Added original value tracking to dataBinder classes"  This reverts commit 01bb493c3176df821ce713c3af869d5a3e6ba01c.

--- src/Model/DataBinderValue.php
+++ src/Model/DataBinderValue.php
@@ -20,8 +20,6 @@ class DataBinderValue implements \JsonSerializable {
 
     public $other;
 
-    public $orgValue;
-
     function __construct($value, $label = null, $other = null){
 
         \call_user_func_array([$this, 'set'], func_get_args());
@@ -76,8 +74,6 @@ class DataBinderValue implements \JsonSerializable {
 
         if($this->other) $array['__hz_other'] = $this->other;
 
-        if($this->orgValue) $array['__hz_org_value'] = $this->orgValue;
-
         return $array;
 
     }
@@ -127,12 +123,7 @@ class DataBinderValue implements \JsonSerializable {
 
     public function set($value, $label = null, $other = null){
 
-        $orgValue = null;
-
         if(is_array($value)){
-
-            if(array_key_exists('__hz_org_value', $value))
-                $orgValue = $value['__hz_org_value'];
 
             if(array_key_exists('__hz_other', $value))
                 $other = $value['__hz_other'];
@@ -144,9 +135,6 @@ class DataBinderValue implements \JsonSerializable {
                 $value = $value['__hz_value'];
 
         }elseif($value instanceof \stdClass){
-
-            if(property_exists($value, '__hz_org_value'))
-                $orgValue = $value->__hz_org_value;
 
             if(property_exists($value, '__hz_other'))
                 $other = $value->__hz_other;
@@ -164,8 +152,6 @@ class DataBinderValue implements \JsonSerializable {
         $this->label = $label;
 
         $this->other = $other;
-
-        $this->orgValue = $orgValue;
 
     }
 
