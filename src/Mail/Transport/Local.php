11a194fcfc0699b8b9b8616561b10d4f9f18544f
Jamie Carl <jamie@hazaar.io>
2024-05-15 14:56:34 +1000

    Moved MIME rendering into the transports
    This will allow other transports that don't want MIME rendering to be able to get access to un-encoded messages.

--- src/Mail/Transport/Local.php
+++ src/Mail/Transport/Local.php
@@ -2,68 +2,70 @@
 
 namespace Hazaar\Mail\Transport;
 
-class Local extends \Hazaar\Mail\Transport {
+use Hazaar\Mail\Transport;
 
-    public function send($to, $subject = NULL, $message = NULL, $headers = [], $dsn_types = []) {
+class Local extends Transport
+{
+    public function send(
+        array $to,
+        ?string $subject = null,
+        $message = null,
+        array $headers = [],
+        array $attachments = []
+    ): bool {
+        $sendmailFrom = '';
 
-        $sendmail_from = '';
+        $mailHeaders = [];
 
-        $mail_headers = [];
-
-        foreach($headers as $key => $value) {
-
-            if(!$value)
+        foreach ($headers as $key => $value) {
+            if (!$value) {
                 continue;
+            }
 
-            if(strtolower($key) == 'from') {
-
+            if ('from' == strtolower($key)) {
                 /*
                  * If this is the from header, try and extract the email address to use in sendmail -f.
                  * Sometimes emails will not send correctly if this fails
                  */
-                if(preg_match('/[\w\s]*\<(.*)\>/', $value, $matches))
-                    $sendmail_from = $matches[1];
-                else
-                    $sendmail_from = $value;
-
+                if (preg_match('/[\w\s]*\<(.*)\>/', $value, $matches)) {
+                    $sendmailFrom = $matches[1];
+                } else {
+                    $sendmailFrom = $value;
+                }
             }
 
-            $mail_headers[] = $key . ':   ' . trim($value);
-
+            $mailHeaders[] = $key.':   '.trim($value);
         }
 
-        $mail_headers = implode("\n", $mail_headers);
+        $mailHeaders = implode("\n", $mailHeaders);
 
         $params = [
-            '-R' => 'hdrs'
+            '-R' => 'hdrs',
         ];
 
-        if($sendmail_from)
-            $params['-f'] = $sendmail_from;
+        if ($sendmailFrom) {
+            $params['-f'] = $sendmailFrom;
+        }
 
-        if(is_array($dsn_types) && count($dsn_types) > 0)
-            $params['-N'] = '"' . implode(',', array_map('strtolower', $dsn_types)) . '"';
+        if (is_array($this->dsn) && count($this->dsn) > 0) {
+            $params['-N'] = '"'.implode(',', array_map('strtolower', $this->dsn)).'"';
+        }
 
-        //The @ sign causes errors not to be thrown and allows things to continue.  the mail() command
-        //will just return false when not successful.
-        $ret = @mail($this->formatTo($to), $subject, $message, $mail_headers, (count($params) > 0 ? array_flatten($params, ' ', ' ') : NULL));
+        // The @ sign causes errors not to be thrown and allows things to continue.  the mail() command
+        // will just return false when not successful.
+        $ret = @mail($this->formatTo($to), $subject, $message, $mailHeaders, count($params) > 0 ? array_flatten($params, ' ', ' ') : null);
 
-        if(!$ret){
-
+        if (!$ret) {
             $error = error_get_last();
 
             throw new Exception\FailConnect(ake($error, 'message'), ake($error, 'type'));
-
         }
 
         return $ret;
-
     }
 
-    private function formatTo($tolist) {
-
+    private function formatTo($tolist)
+    {
         return implode(', ', $tolist);
-
     }
-
 }
