7209253267d9b75a5129c8bc0567e1ea9ac886c8
Jamie Carl <jamie@hazaar.io>
2024-06-05 23:12:45 +1000

    fix(model): error when toArray accesses uninitialised properties
    This commit also has a lot of clean up.
    *  I have changed the way the properties are initialised so that defined properties are now the source, data is then scanned if it exists.  User properties are then processed. This should be more efficient.
    * I have added better type conversion to user properties.
    * I fixed up the object hooks so that serialize, serialized, extend, extended, etc all work correctly.  I added populate hooks.

---
+++ src/Model/Exception/DefineEventHookException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hazaar\Model\Exception;
+
+use Hazaar\Exception;
+
+class DefineEventHookException extends Exception
+{
+    public function __construct(string $class, string $hookName, ?string $reason = null)
+    {
+        parent::__construct("Cannot define event hook '{$hookName}' on class {$class}".($reason ? ": {$reason}" : ''));
+    }
+}
