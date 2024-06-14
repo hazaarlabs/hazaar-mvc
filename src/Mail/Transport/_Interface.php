5c831a4e78521c55f9380732a3615458b045c820
Jamie Carl <jamie@hazaar.io>
2024-06-14 17:01:03 +1000

    feat(mail): recipients now sent to transport as array

--- src/Mail/Transport/_Interface.php
+++ src/Mail/Transport/_Interface.php
@@ -7,10 +7,12 @@ interface _Interface
     /**
      * Send an email via the transport.
      *
+     * @param array{to: array<mixed>, bcc: array<mixed>, cc: array<mixed>} $recipients The email recipients
      * @param array<array<string>|string> $to The email address to send the email to
      */
     public function send(
-        array $to,
+        array $recipients,
+        array $from,
         ?string $subject = null,
         $message = null,
         array $headers = [],
