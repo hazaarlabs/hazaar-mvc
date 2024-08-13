9e33acd8c55158d046a77a613b06a04c916df41a
Jamie Carl <jamie.carl@apir.com.au>
2024-08-13 05:43:27 +0000

    chore(warlock): renamed new Trigger class to REST.

--- src/Warlock/REST.php
+++ src/Warlock/REST.php
@@ -9,7 +9,7 @@ use Hazaar\HTTP\Request;
 use Hazaar\HTTP\Response;
 use Hazaar\Map;
 
-class Trigger
+class REST
 {
     public Config $serverConfig;
     private Client $client;
@@ -28,7 +28,37 @@ class Trigger
         $this->protocol = new Protocol((string) $this->serverConfig['sys']->id, $this->serverConfig['client']['encoded']);
     }
 
-    public function send(string $event, mixed $data = null, mixed $options = null, ?Response &$response = null): bool
+    /**
+     * Triggers an event and sends the corresponding data.
+     *
+     * @param string        $event     the event identifier
+     * @param mixed         $data      The data to be sent with the event. Default is null.
+     * @param mixed         $options   Additional options for the event. Default is null.
+     * @param null|Response &$response A reference to a Response object that will be populated. Default is null.
+     *
+     * @return bool returns true if the event was successfully triggered, false otherwise
+     */
+    public function trigger(string $event, mixed $data = null, mixed $options = null, ?Response &$response = null): bool
+    {
+        return $this->send('TRIGGER', ['id' => $event, 'data' => $data], $response);
+    }
+
+    /**
+     * Sends a request to the configured server.
+     *
+     * This method constructs a URL based on the server configuration and sends a POST request
+     * with the specified type and data. It handles cases where the client server or port is not
+     * specified by defaulting to the server's listen address and port. Additionally, it sets up
+     * the authorization header if an admin key is provided.
+     *
+     * @param string        $type      the type of the request
+     * @param mixed         $data      The data to be sent with the request. Default is null.
+     * @param mixed         $options   Additional options for the request. Default is null.
+     * @param null|Response &$response The response object that will be populated with the server's response. Default is null.
+     *
+     * @return bool returns true if the response status is 200, otherwise false
+     */
+    private function send(string $type, mixed $data = null, mixed $options = null, ?Response &$response = null): bool
     {
         if (null === $this->serverConfig['client']['port']) {
             $this->serverConfig['client']['port'] = $this->serverConfig['server']['port'];
@@ -54,10 +84,7 @@ class Trigger
         if (null !== $this->serverConfig['admin']['key']) {
             $request->setHeader('Authorization', 'Apikey '.base64_encode($this->serverConfig['admin']['key']));
         }
-        $request->setBody($this->protocol->encode('TRIGGER', [
-            'id' => $event,
-            'data' => $data,
-        ]));
+        $request->setBody($this->protocol->encode($type, $data));
         $response = $this->client->send($request);
 
         return 200 === $response->status;
