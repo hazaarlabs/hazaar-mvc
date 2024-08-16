36cdcea4788f29a6ca3366b5c70889b069bea259
Jamie Carl <jamie@hazaar.io>
2024-08-16 17:24:47 +1000

    fix(stan): added type hint in test routes

--- tests/application/route.php
+++ tests/application/route.php
@@ -1,5 +1,8 @@
 <?php
 
 use Application\Controllers\Test;
+use Hazaar\Application\Router\Custom as Router;
 
+/** @var Router $route */
 $route->get('/test/{word}', [Test::class, 'bar']);
+$route->get('/test', [Test::class, 'foo']);
