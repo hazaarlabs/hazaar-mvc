52faa3abd0ef1c8cb21d46871c5faa77cd0d1655
Jamie Carl <jamie@hazaar.io>
2024-09-23 11:00:25 +1000

    feat(ratelimit): added support for btree file compact on interval

--- src/RateLimiter/Backend/File.php
+++ src/RateLimiter/Backend/File.php
@@ -3,7 +3,7 @@
 namespace Hazaar\RateLimiter\Backend;
 
 use Hazaar\Application;
-use Hazaar\BTree;
+use Hazaar\File\BTree;
 use Hazaar\RateLimiter\Backend;
 
 class File extends Backend
@@ -14,6 +14,8 @@ class File extends Backend
      * @var array<string,array<mixed>>
      */
     private array $index = [];
+    private ?int $created = null;
+    private int $compactInterval = 300;
 
     /**
      * File rate limiter constructor.
@@ -26,6 +28,10 @@ class File extends Backend
             throw new \Exception('Application not initialized!');
         }
         $this->db = new BTree($app->getRuntimePath(ake($options, 'file', 'rate_limiter.db')));
+        if (!($this->created = $this->db->get('created'))) {
+            $this->db->set('created', $this->created = time());
+        }
+        $this->compactInterval = ake($options, 'compactInterval', $this->compactInterval);
     }
 
     public function __destruct()
@@ -36,7 +42,9 @@ class File extends Backend
         foreach ($this->index as $identifier => $info) {
             $this->db->set($identifier, $info);
         }
-        // $this->db->compact();
+        if (null !== $this->created && $this->created > (time() - $this->compactInterval)) {
+            $this->db->compact();
+        }
     }
 
     public function check(string $identifier): array
