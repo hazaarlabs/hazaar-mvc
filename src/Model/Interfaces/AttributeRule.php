2186f17b5b0f209b08298b2ca4314b94e57de86f
Jamie Carl <jamie@hazaar.io>
2024-12-16 23:09:15 +1100

    Resolve "Feature Request: Upgrade `Model` Class to Use PHP Attributes"

---
+++ src/Model/Interfaces/AttributeRule.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hazaar\Model\Interfaces;
+
+interface AttributeRule
+{
+    /**
+     * Evaluate the sttrinbute rule against the value.
+     *
+     * @param mixed               $value    the value to evaluate
+     * @param \ReflectionProperty $property the property being evaluated
+     *
+     * @return bool true if the value passes the rule, false otherwise
+     */
+    public function evaluate(mixed &$value, \ReflectionProperty &$property): bool;
+}
