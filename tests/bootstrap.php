d170c2d4ed13a3ebd94314f6a137771a3f138c2b
Jamie Carl <jamie@hazaar.io>
2024-12-17 10:58:02 +1100

    chore(legacy): removed CI from legacy
    I don't want to use my resources testing or pipelining legacy code anymore.

--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('APPLICATION_PATH', getcwd() . '/tests/application');
-require_once ('src/HelperFunctions.php');
-//Set up the $_SERVER variable with some stuff that we need and can then predict.
-$_SERVER['SERVER_NAME'] = 'www.example.com';
-$_SERVER['SERVER_PORT'] = 80;
-$_SERVER['REQUEST_URI'] = '/test';
