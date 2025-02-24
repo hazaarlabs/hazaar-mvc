1c4c122dda695731a3d8fc4ba6cbfd4909defb20
Jamie Carl <jamie@hazaar.io>
2025-02-24 11:10:48 +1100

    feat(dbi): added SQLite driver
    With tests.  Currently table queries are working.  I will need to investigate what other features are available (transactions, views, functions, etc) and add them.

---
+++ src/DBI/DBD/Sqlite.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hazaar\DBI\DBD;
+
+use Hazaar\DBI\DBD\Interface\Driver;
+use Hazaar\DBI\Interface\API\SQL;
+
+class Sqlite implements Driver, SQL
+{
+    use Traits\PDO {
+        Traits\PDO::query as pdoQuery; // Alias the trait's query method to pdoQuery
+    }
+    use Traits\SQL;
+    use Traits\SQL\Table;
+    use Traits\SQL\Index;
+
+    /**
+     * @var array<string>
+     */
+    public static array $dsnElements = [
+        'database',
+    ];
+
+    /**
+     * @param array<mixed> $config
+     */
+    public function __construct(array $config)
+    {
+        $this->initQueryBuilder();
+        if (!array_key_exists('database', $config)) {
+            throw new \Exception('Database not specified in configuration');
+        }
+        $this->connect('sqlite:'.$config['database']);
+    }
+
+    public function repair(): bool
+    {
+        $sql = 'VACUUM ANALYZE';
+
+        return false !== $this->exec($sql);
+    }
+}
---
+++ tests/DBI/SQLiteTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hazaar\Tests\DBI;
+
+use Hazaar\DBI\Adapter;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ */
+class SQLiteTest extends TestCase
+{
+    private Adapter $db;
+
+    // setup
+    public function setUp(): void
+    {
+        $this->db = new Adapter(['type' => 'sqlite', 'database' => ':memory:']);
+        $this->db->exec('CREATE TABLE IF NOT EXISTS test_table (id INTEGER PRIMARY KEY, name TEXT, stored BOOLEAN DEFAULT TRUE)');
+    }
+
+    // teardown
+    public function tearDown(): void
+    {
+        $this->db->exec('DROP TABLE IF EXISTS test_table');
+        unset($this->db);
+    }
+
+    public function testRawSQL(): void
+    {
+        $db = new Adapter(['type' => 'sqlite', 'database' => ':memory:']);
+        $db->exec('CREATE TABLE test_table (id INTEGER PRIMARY KEY, name TEXT, stored BOOLEAN DEFAULT TRUE)');
+        $inserted = $db->exec('INSERT INTO test_table (name) VALUES ("test")');
+        $this->assertEquals(1, $inserted);
+        $result = $db->query('SELECT * FROM test_table');
+        $this->assertIsObject($result);
+        // $this->assertEquals(1, $result->count());
+        $row = $result->fetch();
+        $this->assertIsArray($row);
+        $this->assertEquals('test', $row['name']);
+        unset($row, $result); // Unlock the result set so we can drop the table
+    }
+
+    public function testTableQueries(): void
+    {
+        //Setup
+        $table = $this->db->table('test_table');
+        $this->assertInstanceOf('Hazaar\DBI\Table', $table);
+        $this->assertEquals('test_table', $table->getName());
+        //Insert
+        $rowId = $table->insert(['name' => 'test'], 'id');
+        $this->assertIsInt($rowId);
+        $this->assertGreaterThan(0, $rowId);
+        //Select
+        $result = $table->findOne(['id' => $rowId]);
+        $this->assertIsArray($result);
+        $this->assertEquals('test', $result['name']);
+        //Update
+        $updated = $table->update(['name' => 'test2'], ['id' => $rowId]);
+        $this->assertEquals(1, $updated);
+        $result = $table->findOne(['id' => $rowId]);
+        $this->assertIsArray($result);
+        $this->assertEquals('test2', $result['name']);
+        //Delete
+        $deleted = $table->delete(['id' => $rowId]);
+        $this->assertEquals(1, $deleted);
+        $result = $table->findOne(['id' => $rowId]);
+        $this->assertFalse($result);
+    }
+}
