b2cbbcf0942fbb8b61dd063a07e1609aa390a5a8
Jamie Carl <jamie@hazaar.io>
2025-03-06 11:26:19 +1100

    chore(test): re-enabled apc and shm cache tests

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -39,23 +39,23 @@ class CacheTest extends TestCase
         $this->assertEquals('value', $cache->get('test'));
     }
 
-    // public function testSHMCached(): void
-    // {
-    //     $options = [
-    //         'namespace' => 'test',
-    //     ];
-    //     $cache = new Adapter('shm', $options);
-    //     $this->assertTrue($cache->set('test', 'value'));
-    //     $this->assertEquals('value', $cache->get('test'));
-    // }
+    public function testSHMCached(): void
+    {
+        $options = [
+            'namespace' => 'test',
+        ];
+        $cache = new Adapter('shm', $options);
+        $this->assertTrue($cache->set('test', 'value'));
+        $this->assertEquals('value', $cache->get('test'));
+    }
 
-    // public function testAPCCached(): void
-    // {
-    //     $options = [
-    //         'namespace' => 'test',
-    //     ];
-    //     $cache = new Adapter('apc', $options);
-    //     $this->assertTrue($cache->set('test', 'value'));
-    //     $this->assertEquals('value', $cache->get('test'));
-    // }
+    public function testAPCCached(): void
+    {
+        $options = [
+            'namespace' => 'test',
+        ];
+        $cache = new Adapter('apc', $options);
+        $this->assertTrue($cache->set('test', 'value'));
+        $this->assertEquals('value', $cache->get('test'));
+    }
 }
