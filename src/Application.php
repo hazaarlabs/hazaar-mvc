cfe4876fd3bba86559a78bfc45b2f0fcfc2ca16b
Jamie Carl <jamie@hazaar.io>
2025-03-10 21:02:50 +1100

    chore(app): moved error functions from root namespace

--- src/Application.php
+++ src/Application.php
@@ -15,6 +15,7 @@ declare(strict_types=1);
 namespace Hazaar;
 
 use Hazaar\Application\Config;
+use Hazaar\Application\Error;
 use Hazaar\Application\FilePath;
 use Hazaar\Application\Request;
 use Hazaar\Application\Router;
@@ -27,8 +28,6 @@ use Hazaar\Util\Arr;
 use Hazaar\Util\Timer;
 
 require_once __DIR__.DIRECTORY_SEPARATOR.'Constants.php';
-
-require_once __DIR__.DIRECTORY_SEPARATOR.'ErrorControl.php';
 
 define('HAZAAR_VERSION', '3.0');
 define('HAZAAR_START', microtime(true));
@@ -128,7 +127,7 @@ class Application
             $this->router = new Router($routerConfig);
             $this->timer->stop('init');
         } catch (\Throwable $e) {
-            dieDieDie($e);
+            Error::dieDieDie($e);
         }
     }
 
@@ -545,7 +544,7 @@ class Application
                 ob_end_clean();
             }
         } catch (Controller\Exception\HeadersSent $e) {
-            dieDieDie('HEADERS SENT');
+            Error::dieDieDie('HEADERS SENT');
         } catch (\Throwable $e) {
             /*
             * Here we check if the controller we tried to execute was already an error
@@ -553,7 +552,7 @@ class Application
             * so we throw a normal exception that will be grabbed by ErrorControl as an unhandled exception.
             */
             if ($controller instanceof Controller\Error) {
-                dieDieDie($e->getMessage());
+                Error::dieDieDie($e->getMessage());
             } else {
                 $this->exceptionHandler($e, isset($route) ? $route->getResponseType() : null);
             }
@@ -638,7 +637,7 @@ class Application
             Frontend::e('CORE', "Error #{$errno} on line {$errline} of file {$errfile}: {$errstr}");
         }
 
-        errorAndDie($errno, $errstr, $errfile, $errline, debug_backtrace());
+        Error::errorAndDie($errno, $errstr, $errfile, $errline, debug_backtrace());
 
         return true;
     }
@@ -656,7 +655,7 @@ class Application
                 ob_clean();
             }
             match ($error['type']) {
-                E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR => errorAndDie($error, debug_backtrace()),
+                E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR => Error::errorAndDie($error, debug_backtrace()),
                 default => null
             };
         }
@@ -675,7 +674,7 @@ class Application
         if ($e->getCode() >= 500) {
             Frontend::e('CORE', 'Error #'.$e->getCode().' on line '.$e->getLine().' of file '.$e->getFile().': '.$e->getMessage());
         }
-        errorAndDie($e, $responseType);
+        Error::errorAndDie($e, $responseType);
     }
 
     public function getBase(): string
---
+++ src/Application/Error.php
@@ -0,0 +1,146 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hazaar\Application;
+
+use Hazaar\Application;
+use Hazaar\Controller\Response\Stream;
+use Hazaar\HTTP\Response;
+use Hazaar\Util\Arr;
+use Hazaar\Util\Boolean;
+
+class Error
+{
+    /**
+     * This function handles errors and terminates the script execution.
+     */
+    public static function errorAndDie(): void
+    {
+        $code = 0;
+        $app = Application::getInstance();
+        $headers = Arr::unflatten(headers_list(), ':', "\n");
+        $args = func_get_args();
+        if ('stream' == ($headers['X-Response-Type'] ?? null)) {
+            $stream = new Stream($args[0]);
+            $stream->writeOutput();
+        } elseif ($app instanceof Application && isset($app->router)) {
+            $controller = $app->router->getErrorController();
+            call_user_func_array([$controller, 'setError'], $args);
+            $controller->cleanOutputBuffer();
+            $code = $app->run($controller);
+        } else {
+            $error = [10500, 'An unknown error occurred!', __FILE__, __LINE__, null, []];
+            if (count($args) > 0) {
+                if (
+                    $args[0] instanceof \Exception
+                    || $args[0] instanceof Error
+                ) {
+                    $error = [
+                        'code' => $args[0]->getCode(),
+                        'message' => $args[0]->getMessage(),
+                        'file' => $args[0]->getFile(),
+                        'line' => $args[0]->getLine(),
+                        'context' => null,
+                        'trace' => $args[0]->getTrace(),
+                    ];
+                } elseif (isset($args[0]) && is_array($args[0]) && array_key_exists('type', $args[0])) {
+                    $error = [
+                        'code' => $args[0]['type'],
+                        'message' => $args[0]['message'],
+                        'file' => $args[0]['file'],
+                        'line' => $args[0]['line'],
+                        'trace' => isset($args[1]) ? $args[1] : null,
+                    ];
+                } else {
+                    $error = [
+                        'code' => $args[0],
+                        'message' => $args[1],
+                        'file' => $args[2],
+                        'line' => $args[3],
+                        'trace' => isset($args[4]) ? $args[4] : null,
+                    ];
+                }
+                if ('cli' === php_sapi_name()) {
+                    $die = "##############################\n# Hazaar - Console Error #\n##############################\n\n";
+                    $die .= "{$error['message']}\n\n";
+                    if (!is_array($error['trace'])) {
+                        $error['trace'] = debug_backtrace();
+                    }
+                    $die .= "Call stack:\n\n";
+                    foreach ($error['trace'] as $x => $trace) {
+                        $die .= count($error['trace']) - $x.'. '
+                            .(array_key_exists('class', $trace) ? ". {$trace['class']}->" : '')
+                            .(array_key_exists('function', $trace) ? "{$trace['function']}" : '')
+                            .(array_key_exists('file', $trace) ? "{$trace['file']}:{$trace['line']}\n" : '');
+                    }
+
+                    exit($die);
+                }
+            }
+            echo 'FATAL ERROR';
+            var_dump($error);
+            $code = $error['code'];
+        }
+
+        exit($code);
+    }
+
+    /**
+     * Terminates the script execution and displays an error message.
+     *
+     * This function is used to handle errors and terminate the script execution. It clears
+     * the output buffer, sets the HTTP response code to 500, and displays an error message
+     * with the provided error information.
+     *
+     * @param string|\Throwable $err The error message or Throwable object
+     */
+    public static function dieDieDie(string|\Throwable $err): void
+    {
+        while (count(ob_get_status()) > 0) {
+            ob_end_clean();
+        }
+        $code = 500;
+        $errString = 'An unknown error has occurred';
+        if ($err instanceof \Throwable) {
+            $errString = $err->getMessage();
+            if (Boolean::from(ini_get('display_errors'))) {
+                $errString .= "\n\non line ".$err->getLine().' of file '.$err->getFile()."\n\n".$err->getTraceAsString();
+            }
+        } elseif (is_string($err)) {
+            $errString = $err;
+        }
+        if ('cli' === php_sapi_name()) {
+            $msg = "Hazaar ERROR: {$errString}\n";
+        } else {
+            http_response_code($code);
+            $msg = '<h1>'.Response::getText(http_response_code())."</h1><pre>{$errString}</pre>"
+                .'<hr/><i>Hazaar/'.HAZAAR_VERSION
+                .' ('.php_uname('s').')';
+            if (array_key_exists('SERVER_NAME', $_SERVER)) {
+                $msg .= ' Server at '.$_SERVER['SERVER_NAME'].' Port '.$_SERVER['SERVER_PORT'].'</i>';
+            }
+        }
+
+        exit($msg);
+    }
+
+    /**
+     * Traces the current execution and terminates the script.
+     *
+     * This function generates a backtrace of the current execution and includes a trace file for debugging purposes.
+     * After generating the trace, it terminates the script using the `exit` function.
+     */
+    public static function traceAndDie(): void
+    {
+        $trace = debug_backtrace();
+
+        include realpath(__DIR__
+            .DIRECTORY_SEPARATOR.'..'
+            .DIRECTORY_SEPARATOR.'libs'
+            .DIRECTORY_SEPARATOR.'error'
+            .DIRECTORY_SEPARATOR.'trace.php');
+
+        exit;
+    }
+}
