fc22711d3b61cbc1bafaeab89a0c15acb0c732db
Jamie Carl <jamie@hazaar.io>
2025-04-15 20:40:03 +1000

    feat(util): Adds GeoData module to cache database file
    Adds a new GeoData module that enables caching of the GeoData database file.
    
    This feature allows users to cache the geodata database file locally,
    improving performance and reducing reliance on external data sources.
    The module includes a command to fetch and cache the database file,
    as well as display relevant information such as file size and location.
    
    Also refactors the GeoData class to be more flexible and to fetch the DB file.

---
+++ src/Console/Modules/GeoDataModule.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Hazaar\Console\Modules;
+
+use Hazaar\Console\Input;
+use Hazaar\Console\Module;
+use Hazaar\Console\Output;
+use Hazaar\Util\GeoData;
+use Hazaar\Util\Str;
+
+class GeoDataModule extends Module
+{
+    protected function configure(): void
+    {
+        $this->addCommand('cache')
+            ->setDescription('Cache the geodata database file')
+        ;
+    }
+
+    protected function execute(Input $input, Output $output): int
+    {
+        try {
+            $output->write('<fg=green>Fetching geodata database file...</>'.PHP_EOL);
+            $dbFile = GeoData::fetchDBFile();
+            $output->write('<fg=green>GeoData database file cached successfully.</>'.PHP_EOL);
+            $output->write('<fg=green>Database file: '.$dbFile.'</>'.PHP_EOL);
+            $output->write('<fg=green>Database file size: '.Str::fromBytes($dbFile->size()).' bytes</>'.PHP_EOL);
+        } catch (\Exception $e) {
+            throw new \Exception('Failed to cache the geodata database file: '.$e->getMessage());
+        }
+
+        return 0;
+    }
+}
